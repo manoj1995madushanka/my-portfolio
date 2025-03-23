61d69902ac4e1b356d4123e2403ba9cd36f664e6
manoj1995madushanka <madushankamanoj414@gmail.com>
2025-03-23 18:53:45 +0530

    add project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,27 @@
-import {BrowserRouter} from "react-router-dom";
-import Navbar from "./Navbar.jsx";
-import Hero from "./Hero.jsx";
-import About from "./About.jsx";
-import Experience from "./Experience.jsx";
-import React from "react";
-import Works from "./Works.jsx";
-import Feedbacks from "./Feedbacks.jsx";
-import Contact from "./Contact.jsx";
-import StarsCanvas from "./StarsCanvas.jsx";
-import Tech from "./Tech.jsx";
+import { BrowserRouter } from "react-router-dom";
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
-
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar/>
-          <Hero/>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
         </div>
-        <About/>
-        <Experience/>
-        <Tech/>
-        <Works/>
-        <Feedbacks/>
-        <div className="relative z-0">
-          <Contact/>
-          <StarsCanvas/>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
